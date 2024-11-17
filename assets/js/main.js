ecf8f6ef63b46e1ad95db0cb495e01c9e8558704
Matheus-c137 <mateusfreiitas99@gmail.com>
2024-11-17 17:55:17 -0300

    atualização sem imagens

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+function menuHamburguer() {
+    let menuListMobile = document.querySelector('.menu-mobile')
+    if (menuListMobile.classList.contains('menu-mobile-ativado')) {
+        menuListMobile.classList.remove('menu-mobile-ativado')
+        document.querySelector('#icone-menu').src = 'assets/images/menu-branco.svg'
+    } else {
+        menuListMobile.classList.add('menu-mobile-ativado')
+        menuListMobile.style.backgroundImage = 'linear-gradient(90deg, #1E1E1E, #0C1E3B)'
+        document.querySelector('#icone-menu').src ='assets/images/fechar-menu-branco.svg'
+    }
+}
